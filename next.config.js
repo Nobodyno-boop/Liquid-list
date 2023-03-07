91bf9b0e1538f3a8415bcaa5f62e5e5e0237c98f
Nobody <17411508+Nobodyno-boop@users.noreply.github.com>
2023-03-07 17:39:41 +0100

    Ci: fix ci URLs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
 
+const repo = 'liquid-list'
+
 const nextConfig = {
-  assetPrefix: isProd ? '/liquid-list/' : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
+  basePath: isProd ? `/${repo}` : '',
   reactStrictMode: true
 }
 
