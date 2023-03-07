949d1fb91bce6ffd4196ecb0671452bb89cd421c
Nobodyno-boop <17411508+Nobodyno-boop@users.noreply.github.com>
2023-03-07 16:21:17 +0000

    Deploying to gh-pages from @ Nobodyno-boop/Liquid-list@1ce5de0d45aa2fb883525af6116e3acb52e81619 🚀

---
+++ _next/static/_CxLb2eqz-FAqkQoUNajL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-407666ae4bf4a76b.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],"/list":["static/chunks/pages/list-ec2a9266f1d173c8.js"],sortedPages:["/","/_app","/_error","/list"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
