a6ea9b7abf52b7592e0cd30c804eec2e5e8798c5
Charlotte <charlotte.allanic92@gmail.com>
2023-03-07 17:13:43 +0100

    Select tag (WIP)

--- components/molecules/ToDoTags.tsx
+++ components/molecules/ToDoTags.tsx
@@ -5,6 +5,7 @@ import {TagBadge} from "@/components/atoms/TagBadge";
 export const TodoTags = ({initTags}) => {
     const allTags = useStore(tags);
     const [todoTags, setTodoTags] = useState(initTags);
+    const [tagName, setTagName] = useState("")
 
     const addTodoTag = (id) => {
         const todoTag = todoTags.find((t) => t._id === id);
@@ -18,18 +19,32 @@ export const TodoTags = ({initTags}) => {
       setTodoTags(todoTags.filter((tag) => tag._id !== id));
     };
 
+    const newTag = () => {
+        addTag(tagName);
+        const newTag = allTags.find((t) => t.name === tagName);
+        addTodoTag(newTag._id);
+    }
+
     return (
         <div className="card-actions justify-end">
             { todoTags.map((tag, index) => <TagBadge key={tag._id} tag={tag} deleteTag={deleteTag}/>) }
-            <select
-                className="select select-bordered select-primary select-xs w-full max-w-xs"
-                onChange={(e) => addTodoTag(e.target.value)}
-            >
-                <option value={""}></option>
-                {allTags.map(
-                    (tag) => <option key={tag._id} value={tag._id}>{tag.name}</option>
-                )}
-            </select>
+            <div className="dropdown dropdown-hover">
+                <label tabIndex={0} className="btn m-1">Add tag</label>
+                <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                    <li>
+                        <imput type="text" onChange={(e) => setTagName(e.target.value)}/> <button onClick={newTag}>+</button>
+                    </li>
+                        {
+                        allTags.map((tag) =>
+                            <li
+                                key={tag._id}
+                                onClick={() => addTodoTag(tag._id)}
+                            >
+                                <a>{tag.name}</a>
+                            </li>)
+                    }
+                </ul>
+            </div>
         </div>
     )
 }
