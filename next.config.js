6cad13310afa6e1d0ced231223ea352b0f643177
Nobody <17411508+Nobodyno-boop@users.noreply.github.com>
2023-03-07 17:31:52 +0100

    Ci: fix url name

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-  assetPrefix: isProd ? '/Liquid-list/' : '',
+  assetPrefix: isProd ? '/liquid-list/' : '',
   reactStrictMode: true
 }
 
